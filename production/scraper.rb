443ba0b3318bdaeaf2dc262549fc92ce910640d9
Mathias Benjaminsen <mabenjam@gmail.com>
2014-11-28 23:52:32 +0100

    Add more functionality and conditions to scraper

--- production/scraper.rb
+++ production/scraper.rb
@@ -13,21 +13,52 @@ class Scraper
 
     if url_params =~ URI::regexp
       mechanize = Mechanize.new
-      page = mechanize.get(req.params["url"])
+      @page = mechanize.get(req.params["url"])
+
+      url = req.params["url"]
+
+      if @page.at('meta[@property="og:title"]').nil?
+        title = @page.title
+      else
+        title = @page.at('meta[@property="og:title"]')[:content]
+      end
+
+      if @page.at('meta[@property="og:site_name"]').nil?
+        site_name = @page.title
+      else
+        site_name = @page.at('meta[@property="og:site_name"]')[:content]
+      end
+
+      if @page.at('meta[@property="og:description"]').nil?
+        if @page.at('meta[@property="description"]').nil?
+          description = ""
+        else
+          description = @page.at('meta[@property="description"]')[:content]
+        end
+      else
+        description = @page.at('meta[@property="og:description"]')[:content]
+      end
+
+      if @page.at('meta[@property="og:image"]').nil?
+        image = ""
+      else
+        image = @page.at('meta[@property="og:image"]')[:content]
+      end
     end
 
     [
       200,
       {
-        'Content-Type'  => 'application/json'
+        'Content-Type'  => 'application/json',
+        'Access-Control-Allow-Origin' => 'http://localhost:3000'
       },
       [
         {
-          :url => page.at('meta[@property="og:url"]')[:content],
-          :title => page.at('meta[@property="og:title"]')[:content],
-          :name => page.at('meta[@property="og:site_name"]')[:content],
-          :description => page.at('meta[@property="og:description"]')[:content],
-          :image => page.at('meta[@property="og:image"]')[:content]
+          "url" => url,
+          "title" => title,
+          "name" => site_name,
+          "description" => description,
+          "image" => image
         }.to_json
       ]
     ]
